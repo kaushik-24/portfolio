8d74b848c947af75515aa33a651368af78f70e11
kaushik-24 <grg23kaushik@gmail.com>
2025-01-22 15:28:17 +0545

    add type.tsx

---
+++ src/components/ui/Type.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { ReactTyped } from "react-typed";
+import { useMemo } from "react";
+
+const Type = () => {
+  const typedStrings = useMemo(
+    () => ["Kaushik Gurung.", "Front-End Developer."],
+    []
+  );
+
+  return (
+    <ReactTyped
+      strings={typedStrings}
+      typeSpeed={40}
+      backSpeed={50}
+      loop
+      showCursor={true}
+      cursorChar="|"
+    />
+  );
+};
+
+export default Type;
+
